5503eb797b3add719d829112056ca0ffcde9bb28
Alumno <Alumno@PC153>
2024-04-29 11:16:24 -0300

    Método Equilbrar

--- clsArbolBinario.cs
+++ clsArbolBinario.cs
@@ -111,5 +111,80 @@ namespace pryEstructuraDeDatos
                 PreOrden(R.Derecho, NodoPadre);
             }
         }
+
+        public clsNodo BuscarCodigo(Int32 cod)
+        {
+            clsNodo aux = Raiz;
+            while (aux != null)
+            {
+                if (cod == aux.Codigo) break;
+                if (cod < aux.Codigo) aux = aux.Izquierdo;
+                else aux = aux.Derecho;
+            }
+            return aux;
+        }
+
+        private clsNodo[] Vector = new clsNodo[100];
+        private Int32 i = 0;
+
+        public void Equilibrar()
+        {
+            i = 0;
+            GrabarVectorInOrden(Raiz);
+            Raiz = null;
+            EquilibrarArbol(0, i - 1);
+        }
+        
+
+        public void GrabarVectorInOrden(clsNodo NodoPadre)
+        {
+            if (NodoPadre.Izquierdo != null)
+            {
+                GrabarVectorInOrden(NodoPadre.Izquierdo);
+            }
+            Vector[i] = NodoPadre;
+            i = i + 1;
+            if (NodoPadre.Derecho != null)
+            {
+                GrabarVectorInOrden(NodoPadre.Derecho);
+            }
+        }
+
+        public void EquilibrarArbol(Int32 ini, Int32 fin)
+        {
+            Int32 m = (ini + fin) / 2;
+            if (ini <= fin)
+            {
+                Agregar(Vector[m]);
+                EquilibrarArbol(ini, m - 1);
+                EquilibrarArbol(m + 1, fin);
+            }
+        }
+
+        public void Eliminar(Int32 Codigo)
+        {
+            i = 0;
+            GrabarVectorInOrden(Raiz, Codigo);
+            Raiz = null;
+            EquilibrarArbol(0, i - 1);
+        }
+
+        public void GrabarVectorInOrden(clsNodo NodoPadre, Int32 Codigo)
+        {
+            if (NodoPadre.Izquierdo != null)
+            {
+                GrabarVectorInOrden(NodoPadre.Izquierdo, Codigo);
+            }
+            if (NodoPadre.Codigo != Codigo)
+            {
+                Vector[i] = NodoPadre;
+                i = i + 1;
+            }
+            if (NodoPadre.Derecho != null)
+            {
+                GrabarVectorInOrden(NodoPadre.Derecho, Codigo);
+            }
+        }
+
     }
 }
